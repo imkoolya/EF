a2ab9b090d19cb17a6d8acb420adadae17699e11
imkoo <imkoo@VIBIN>
2024-06-04 18:10:29 +0300

    Add project files.

---
+++ EF/AppContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class AppContext : DbContext
+{
+
+    public DbSet<User> Users { get; set; }
+
+    public AppContext()
+    {
+        Database.EnsureCreated();
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Data Source=VIBIN\SQLEXPRESS;Database=EF;Trusted_Connection=True");
+    }
+}
